84e3c9854da62522d018b4a935c4c0735bbbbd5a
David Pinney <david@pinney.org>
2018-12-28 14:27:56 -0500

    Nice v1.0

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 Basic Anomaly Detection from a Forecast that's Monovariate
 
+## Huh?
+
+Have a screenshot:
+
+![Screenshot](screenshot.png)
+
 ## TODO
 
 - XXX data? weather data for lex ky, couple years, from mesonet at iowa state. https://mesonet.agron.iastate.edu/request/download.phtml?network=KY_ASOS
@@ -10,9 +16,12 @@ Basic Anomaly Detection from a Forecast that's Monovariate
 - XXX Just build a forecast on the Lex data.
 - XXX Chart the data vs. forecast bands.
 - XXX Find outliers.
-- OOO Highlight the outliers. https://github.com/facebook/prophet/blob/master/python/fbprophet/plot.py
-- OOO Tune it. https://facebook.github.io/prophet/docs/seasonality,_holiday_effects,_and_regressors.html
-- OOO Refactor in to a clean module.
+- XXX Nicer data model for outliers (stick a flag in the forecast).
+- XXX Label the timestamps.
+- XXX Highlight the outliers. https://github.com/facebook/prophet/blob/master/python/fbprophet/plot.py
+- OOO Refactor in to a clean module. Some kind of object with update(), find_anomalies(), etc.
+- OOO Improve performance?
+- OOO Add in some knobs to tune it. https://facebook.github.io/prophet/docs/seasonality,_holiday_effects,_and_regressors.html
 
 ## Other Options
 
--- badfomo.py
+++ badfomo.py
@@ -5,8 +5,12 @@ from matplotlib import pyplot as plt
 import cPickle as pickle
 import os
 
+FILENAME = 'indata - lex ky temperature.csv'
+BACKUP_NAME = 'out - clean_forecast.csv'
+INTERVAL_WIDTH = 0.99
+
 # Import and clean data.
-df = pd.read_csv('indata - lex ky.csv', comment='#')
+df = pd.read_csv(FILENAME, comment='#')
 df = df[['valid','tmpf']]
 df.rename(inplace=True, columns={"valid": "ds", "tmpf": "y"})
 df.y.replace(to_replace={'M':0.0}, inplace=True)
@@ -14,50 +18,51 @@ df.y.replace(to_replace={'M':0.0}, inplace=True)
 # df.to_csv('clean.csv')
 
 # Train the model.
-pickleName = 'trained lex ky temp model.pickle'
-if not os.path.isfile(pickleName):
+if not os.path.isfile(BACKUP_NAME):
 	print 'TRAINING MODEL!'
 	# Build model.
-	INTERVAL_WIDTH = 0.95
 	m = Prophet(yearly_seasonality=True, daily_seasonality=True, interval_width=INTERVAL_WIDTH)
-	m.add_country_holidays(country_name='US')
+	# m.add_country_holidays(country_name='US')
 	m.fit(df)
-	# Back up the model.
-	with open(pickleName, 'wb') as pickFile:
-		pickle.dump(m, pickFile)
+	# Predict the future.
+	print 'PREDICTING!'
+	future = m.make_future_dataframe(periods=0)
+	forecast = m.predict(future)
+	# Merge in the historical data.
+	forecast['y'] = [float(x) for x in df['y']]
+	forecast['outlier'] = [False for x in xrange(len(forecast))]
+	# Backup the model.
+	forecast.to_csv(BACKUP_NAME)
 else:
-	print 'UNPICKLING MODEL!'
-	with open(pickleName, 'rb') as pickFile:
-		m = pickle.load(pickFile)
-
-# Predict the future.
-print 'PREDICTING!'
-future = m.make_future_dataframe(periods=0)
-forecast = m.predict(future)
-# print forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
-# forecast.to_csv('clean_forecast.csv')
+	print 'LOADING MODEL BACKUP!'
+	forecast = pd.read_csv(BACKUP_NAME)
 
 # Outliers.
-overLiers = []
-underLiers = []
+print 'FINDING OUTLIERS!'
 for index, row in forecast.iterrows():
-	yVal = float(df['y'][index])
+	yVal = float(row['y'])
 	yHigh = float(row['yhat_upper'])
 	yLow = float(row['yhat_lower'])
-	if yVal > yHigh:
-		overLiers.append(row)
-	elif yVal < yLow:
-		underLiers.append(row)
-print 'Frame Rows:', len(forecast.index)
-print 'Outliers:', len(overLiers) + len(underLiers)
+	if (yVal > yHigh) or (yVal < yLow):
+		forecast.at[index,'outlier'] = True
 print 'Interval Width for Outliers:', INTERVAL_WIDTH
-print 'Percent Outlying:', float(len(forecast.index)) / float(len(overLiers) + len(underLiers))
+print 'Frame Rows:', len(forecast.index)
+print 'Percent Outlying:', float(forecast.outlier.sum()) / len(forecast.index)
 
-# Show a forecast.
-print 'PLOTTING!'
-m.plot(forecast)
-plt.show()
-
-# Show components of forecast.
-m.plot_components(forecast);
+# Plot creation and showing.
+fig = plt.figure(facecolor='w', figsize=(10, 6))
+ax = fig.add_subplot(111)
+forecast_t = [np.datetime64(x) for x in forecast['ds']]
+inliers = [(r[1] if not r[0] else None) for r in zip(forecast['outlier'], forecast['y'])]
+outliers = [(r[1] if r[0] else None) for r in zip(forecast['outlier'], forecast['y'])]
+ax.plot(forecast_t, forecast['yhat'], ls='-', c='#0072B2', label='prediction')
+ax.plot(forecast_t, forecast['y'], 'k.', label='measurement')
+ax.plot(forecast_t, outliers, 'r.', label='outlier')
+ax.fill_between(forecast_t, forecast['yhat_lower'], forecast['yhat_upper'], color='#0072B2', alpha=0.2, label='detection interval')
+ax.grid(True, which='major', c='gray', ls='-', lw=1, alpha=0.2)
+ax.set_xlabel('ds')
+ax.set_ylabel('y')
+ax.legend()
+plt.title('Outliers and Model for "' + FILENAME + '"')
+fig.tight_layout()
 plt.show()
